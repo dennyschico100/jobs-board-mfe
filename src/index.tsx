68ae01c458dbf777ae0a22732e96a40386b2a604
dennyscordova100 <dennyschico2@gmail.com>
2025-11-16 12:56:51 -0600

    chore: update way chakra ui wrapped the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from '@/components/ui/provider'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   const root = createRoot(rootElement)
   root.render(
-    <ChakraProvider>
+    <Provider>
       <App />
-    </ChakraProvider>,
+    </Provider>,
   )
 } else {
   console.error('The element #root was not found in index.html')
