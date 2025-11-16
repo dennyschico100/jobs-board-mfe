0bfc785c378154603832af49b69ec066c8b96f3b
dennyscordova100 <dennyschico2@gmail.com>
2025-11-15 21:04:26 -0600

    chore: installing dependencies and configuring webpack

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const App = () => {
+    return (<div><header>JOB BOARD MFE</header></div>)
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    devServer: {
+        port: 3001,
+        hot: true,              // HMR: hot module replacement
+        open: true,             // abre el navegador automáticamente
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+    },
+    output: {
+        publicPath: "http://localhost:3001/",
+        clean: true,
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "job_board",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./App": "./src/App.tsx",
+            },
+            shared: {
+                react: { singleton: true, eager: true },
+                "react-dom": { singleton: true, eager: true },
+            },
+        }),
+        new HtmlWebpackPlugin({ template: "./mfe/public/index.html" }),
+    ],
+};
